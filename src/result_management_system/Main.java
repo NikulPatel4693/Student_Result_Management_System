670a070c5973cb61c88dd65960b5dfaa07f00edf
HP <HP@MAM-Jamin>
2023-07-24 18:16:21 +0530

    first commit

---
+++ src/result_management_system/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package result_management_system;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args){
+        index id = new index();
+    }
+}
